f1813445624e1b598fe53dd462b807a2a6faee19
LingGrani <luthfiangger2510@gmail.com>
2023-04-03 11:16:20 +0700

    Looping input array mhs

--- Structure3/Structure3.cpp
+++ Structure3/Structure3.cpp
@@ -14,3 +14,23 @@ struct Mahasiswa
 	AlamatDetail Alamat;
 	int Umur;
 };
+
+int main() 
+{
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Masukan NIM: ";
+		cin.getline(mhs[i].NIM,12);
+		cout << "Masukan Nama: ";
+		cin.getline(mhs[i].Nama, 25);
+		cout << "Alamat : ";
+		cout << "Masukan Desa: ";
+		cin.getline(mhs[i].Alamat.Desa, 20);
+		cout << "Masukan Kota: ";
+		cin.getline(mhs[i].Alamat.Kota, 20);
+		cout << "Masukan Umur: ";
+		cin >> mhs[i].Umur;
+	}
+}
