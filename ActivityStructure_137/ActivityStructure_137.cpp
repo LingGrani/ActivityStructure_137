28a61cc5fa5d5ab40c036e3c737744134d5ed448
LingGrani <luthfiangger2510@gmail.com>
2023-04-03 10:29:08 +0700

    input mhs1, mhs2, dan display mhs1, mhs2

--- ActivityStructure_137/ActivityStructure_137.cpp
+++ ActivityStructure_137/ActivityStructure_137.cpp
@@ -8,3 +8,33 @@ struct Mahasiswa
 	string Alamat;
 	int Umur;
 };
+
+int main () 
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.NIM = "20220140137";
+	mhs1.Nama = "Luthfi Angger R.";
+	mhs1.Alamat = "Bogor";
+	mhs1.Umur = 18;
+
+	cout << "Masukan NIM: ";
+	cin >> mhs2.NIM;
+	cout << "Masukan Nama: ";
+	cin >> mhs2.Nama;
+	cout << "Masukan Alamat: ";
+	cin >> mhs2.Alamat;
+	cout << "Masukan Umur: ";
+	cin >> mhs2.Umur;
+
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nNama = " << mhs1.Nama;
+	cout << "\nAlamat = " << mhs1.Alamat;
+	cout << "\nUmur = " << mhs1.Umur;
+
+	cout << "\n\nNIM = " << mhs2.NIM;
+	cout << "\nNama = " << mhs2.Nama;
+	cout << "\nAlamat = " << mhs2.Alamat;
+	cout << "\nUmur = " << mhs2.Umur;
+
+}
