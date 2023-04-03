4e460526e03a57fe7da7f242e91aca42f22d931f
LingGrani <luthfiangger2510@gmail.com>
2023-04-03 10:45:10 +0700

    input mhs

--- Structure2/Structure2.cpp
+++ Structure2/Structure2.cpp
@@ -16,5 +16,17 @@ struct Mahasiswa
 
 int main()
 {
+	Mahasiswa mhs;
 
+	cout << "Masukan NIM: ";
+	cin >> mhs.NIM;
+	cout << "Masukan Nama: ";
+	cin >> mhs.Nama;
+	cout << "Masukan Alamat: ";
+	cout << "\tDesa: ";
+	cin >> mhs.Alamat.Desa;
+	cout << "\tKota: ";
+	cin >> mhs.Alamat.Kota;
+	cout << "Masukan Umur: ";
+	cin >> mhs.Umur;
 }
